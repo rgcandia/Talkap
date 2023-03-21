407756b437e978095455a29244322d6d9f74cca8
Roque Alejandro Gonzalez Candia <alejandrogcandia@gmail.com>
2023-03-20 23:05:58 -0300

    configurando puerto

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ const server = require("./src/app.js");
 const { conn } = require("./src/db.js");
 const { initGroup } = require("./src/services.js");
 require("dotenv").config();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3000;
 
 conn.sync({ force: true }).then(() => {
   //Inicializo el Group
