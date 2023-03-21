baa0603729643dab272653bbdad1e52542720f08
Roque Alejandro Gonzalez Candia <alejandrogcandia@gmail.com>
2023-03-20 23:33:41 -0300

    configurando 2

--- server/src/app.js
+++ server/src/app.js
@@ -33,7 +33,7 @@ cloudinary.config({
   httpServer.name = 'API';
   httpServer.use(express.json());
   httpServer.use(morgan('dev'));
-  httpServer.use(cors({origin:'*'}));
+  httpServer.use( cors({ origin: true, credentials: true  }) );
   // httpServer.use((req, res, next) => {
   //   res.header('Access-Control-Allow-Origin', '*'); // update to match the domain you will make the request from
   //   res.header('Access-Control-Allow-Credentials', 'true');
